6508102aaa7bc922e54ca1fd033ec3f397820db3
gcf <994693031@qq.com>
2021-01-01 16:46:52 +0800

    完成了登录模块

---
+++ assets/js/login.js
@@ -0,0 +1,81 @@
+$(function() {
+    $("#link_reg").on("click", function() {
+
+        $(".logbox").hide().siblings(".regbox").show()
+    })
+    $("#link_login").on("click", function() {
+
+        $(".regbox").hide().siblings(".logbox").show()
+    })
+
+
+    let form = layui.form
+    var layer = layui.layer;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function(vaule) {
+            var repwd = $(".regbox [name = password]").val()
+
+            if (repwd != vaule) {
+                return "两次密码输出不一致"
+            }
+
+        }
+    })
+
+    $(".form_reg").on("submit", function(e) {
+        e.preventDefault();
+        var dataS = {
+            username: $(".regbox [name=username]").val(),
+            password: $(".regbox [name=password]").val()
+        }
+        console.log(dataS);
+        $.ajax({
+            type: "POST",
+            url: "/api/reguser",
+            data: dataS,
+            success: function(res) {
+
+                if (res.status != 0) return layer.msg(res.message);
+                layer.msg(res.message, function() {
+                    $("#link_login").click()
+                })
+
+
+            }
+
+        })
+    })
+
+    $(".form_login").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/api/login",
+            data: $(this).serialize(),
+            success: function(res) {
+
+
+                if (status != 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg("登录成功")
+                location.href = "/index.html"
+                localStorage.setItem("token", res.token)
+
+
+
+
+            }
+        })
+
+    })
+
+
+
+
+
+
+
+
+})
